142e54de9c89269b51bbb4474acb4ae470d06287
seroroman <47804854+seroroman@users.noreply.github.com>
2022-01-09 15:59:43 +0900

    router nest

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
+import { Page3 } from "./Page3";
 import "./styles.css";
 
 export default function App() {
@@ -14,17 +17,35 @@ export default function App() {
         <Link to="/page1">Page1</Link>
         <br />
         <Link to="/page2">Page2</Link>
+        <br />
+        <Link to="/page3">Page3</Link>
       </div>
 
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route exact path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route exact path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        ></Route>
         <Route path="/page2">
           <Page2 />
+        </Route>
+        <Route path="/page3">
+          <Page3 />
         </Route>
       </Switch>
     </BrowserRouter>
